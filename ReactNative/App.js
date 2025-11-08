c1e3618b22ffae1074c4466b10a00aa0a3331c88
Bien <2302570@ub.edu.ph>
2025-11-08 14:57:31 +0800

    ADDED FLATLIST

--- ReactNative/App.js
+++ ReactNative/App.js
@@ -1,6 +1,27 @@
 import React from 'react';
-import {ActivityIndicator, StyleSheet} from 'react-native';
+import {ActivityIndicator, StyleSheet, Button, View, Text, Alert, FLatList, StatusBar} from 'react-native';
 import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
+
+const DATA = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'First Item',
+  },
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Second Item',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'Third Item',
+  },
+];
+
+const Item = ({title}) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{title}</Text>
+  </View>
+);
 
 const App = () => (
   <SafeAreaProvider>
@@ -62,6 +83,12 @@ const App = () => (
         </View>
       </View>
 
+      <FlatList
+        data={DATA}
+        renderItem={({item}) => <Item title={item.title} />}
+        keyExtractor={item => item.id}
+      />
+      
     </SafeAreaView>
   </SafeAreaProvider>
 
